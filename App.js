8f96d44f9a00e33ee02cbb52d6b50d7ea5c9df24
underd0g <underd0g.3003@gmail.com>
2019-05-11 15:47:01 -0300

    adding filter in search box

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 import HeaderStyle from './HeaderStyle.js';
 
 const restaurants = [
@@ -16,12 +16,30 @@ const restaurants = [
   {name: 'Taco Place', address: '550 Maple Rd'}
 ];
 export default class App extends Component {
+  state = {
+    search: null
+  };
+
   render() {
     return (
         <View style={{flex: 1}}>
           <Text style={HeaderStyle.header}> Restaurant Review</Text>
+
+          <TextInput
+              style={styles.input}
+              placeholder="Live Search"
+              onChangeText={text => {
+                this.setState({ search: text})
+              }}
+              value={this.state.search}
+          />
+
+
           {
-            restaurants.map((place, index) => {
+            restaurants.filter(place => {
+              return !this.state.search ||
+                  place.name.toLowerCase().indexOf(this.state.search.toLowerCase()) > -1
+            }).map((place, index) => {
               return (
                   <View key={index} style={[
                     styles.row,
@@ -66,5 +84,15 @@ const styles = StyleSheet.create({
   },
   addressText : {
     color: 'grey'
+  },
+  input :{
+    marginBottom: 30,
+    padding: 10,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    color: '#444',
+    borderBottomWidth: 1,
+    borderColor: '#ddd',
+    backgroundColor: '#F5F5F5'
   }
 })
