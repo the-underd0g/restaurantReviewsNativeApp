f2cd9643c33f537c75218c04d5ec3ea224d868be
underd0g <underd0g.3003@gmail.com>
2019-05-11 18:56:52 -0300

    styling list and adding RestaruantRow Component

--- App.js
+++ App.js
@@ -7,14 +7,33 @@
  */
 
 import React, {Component} from 'react';
-import { Platform, View, Text, StyleSheet, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TextInput, FlatList } from 'react-native';
 import Header from 'components/Header';
+import RestaurantRow from 'components/RestaurantRow';
 
 const restaurants = [
   {name: 'React Cafe', address: '123 Anywhere St'},
-  {name: 'Fancy restaurant', address: '123 Anywhere St'},
-  {name: 'Taco Place', address: '550 Maple Rd'}
+  {name: 'Fancy Restaurant', address: '799 Main St'},
+  {name: 'Taco Place', address: '550 Maple Rd'},
+  {name: "Tony's Diner", address: '4101 College St'},
+  {name: 'Pasta Central', address: '706 Harper St'},
+  {name: 'Burger Builder', address: '4869 Hamilton Dr'},
+  {name: 'Pizza Express', address: '1049 Bird St'},
+  {name: 'Teriyaki To Go', address: '1885 Tea Berry Lane'},
+  {name: 'Maroon Deli', address: '1082 Stuart St'},
+  {name: 'Prime Bar and Grill', address: '1848 Fairfax Dr'},
+  {name: 'Dumpling House', address: '747 Kelly Dr'},
+  {name: 'Hot Chicken', address: '1816 Olive St'},
+  {name: "Luna's Tap Room", address: '3256 Spirit Dr'},
+  {name: 'Quick Sandwich Shop', address: '2587 Cherry Ridge Dr'},
+  {name: "Bobby's Burgers", address: '4152 Berkley St'},
+  {name: 'Turnpike Diner', address: '4571 Central Ave'},
+  {name: 'Bombay Express', address: '65 Queens Lane'},
+  {name: 'Coffee Central', address: '3228 Oakwood Circle'},
+  {name: "King's Garden", address: '2935 Victoria Ct'},
+  {name: 'Salads and More', address: '2454 Preston St'},
 ];
+
 export default class App extends Component {
   state = {
     search: null
@@ -33,60 +52,27 @@ export default class App extends Component {
               }}
               value={this.state.search}
           />
-
-
-          {
-            restaurants.filter(place => {
-              return !this.state.search ||
-                  place.name.toLowerCase().indexOf(this.state.search.toLowerCase()) > -1
-            }).map((place, index) => {
-              return (
-                  <View key={index} style={[
-                    styles.row,
-                    { backgroundColor: index % 2 === 0 ? 'white' : '#F3F3F7' }]
-                  }
-                  >
-                    <View style={styles.edges}>
-                      <Text>{index + 1}</Text>
-                    </View>
-
-                    <View style={styles.nameAddress}>
-                      <Text>{place.name}</Text>
-                      <Text style={styles.addressText}> {place.address} </Text>
-                    </View>
-
-                    <View style={styles.edges}>
-                      <Text>Info</Text>
-                    </View>
-
-                  </View>
-              )
-            })
-          }
+            <FlatList
+                data = {
+                    restaurants.filter(place => {
+                        return !this.state.search ||
+                            place.name.toLowerCase().indexOf(this.state.search.toLowerCase()) > -1
+                    })
+                }
+                renderItem={({ item, index }) =>
+                    <RestaurantRow place={item} index={index} />
+                }
+                keyExtractor={item => item.name}
+                initialNumToRender={16}
+                ListHeaderComponent={<View style={{height: 30}} />}
+            />
         </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  row: {
-    flexDirection: 'row'
-  },
-  edges: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 5
-  },
-  nameAddress: {
-    flexDirection: 'column',
-    flex: 8
-  },
-  addressText : {
-    color: 'grey'
-  },
   input :{
-    marginBottom: 30,
     padding: 10,
     paddingHorizontal: 20,
     fontSize: 16,
---
+++ src/components/RestaurantRow.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Button,
+    TouchableOpacity,
+    TouchableHighlight,
+    TouchableWithoutFeedback
+} from 'react-native';
+
+export default class RestaurantRow extends Component {
+    state = {
+        showInfo: false
+    }
+
+    infoPressed = () => {
+        this.setState({ showInfo: !this.state.showInfo })
+    }
+
+    render() {
+
+        const {
+            place,
+            index
+        } = this.props;
+
+        return (
+            <View key={place.name} style={{ backgroundColor: index % 2 === 0 ? 'white' : '#F3F3F7' }}>
+                <View style={styles.row}>
+
+                    <View style={styles.edges}>
+                        <Text>{index + 1}</Text>
+                    </View>
+
+                    <View style={styles.nameAddress}>
+                        <Text>{place.name}</Text>
+                        <Text style={styles.addressText}> {place.address} </Text>
+                    </View>
+
+                    <View style={styles.edges}>
+                        <TouchableHighlight
+                           style={styles.button}
+                            onPress={this.infoPressed}
+                            underlayColor='#5398DC'>
+                            <Text>Info</Text>
+                        </TouchableHighlight>
+                    </View>
+
+                </View>
+
+                {
+                    this.state.showInfo &&
+                    <View style={styles.info}>
+                        <Text style={styles.buttonText}>Restaurant Info</Text>
+                    </View>
+                }
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row'
+    },
+    edges: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 5,
+        minWidth: 50
+    },
+    nameAddress: {
+        flexDirection: 'column',
+        flex: 8
+    },
+    addressText : {
+        color: 'grey'
+    },
+    button : {
+        borderWidth: 1,
+        borderColor: '#0066CC',
+        borderRadius: 14,
+        paddingHorizontal: 10,
+        paddingVertical: 3,
+        backgroundColor: '#fff'
+    },
+    buttonText: {
+        color: '#0066CC',
+        fontSize: 12
+    },
+    info: {
+        marginHorizontal: 40,
+        marginVertical: 10,
+        padding: 10,
+        backgroundColor: '#fff',
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 4
+    }
+});
