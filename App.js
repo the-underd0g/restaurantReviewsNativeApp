3039b36c4b70af1e3b349256d16767f8e1b74c4c
underd0g <underd0g.3003@gmail.com>
2019-05-11 15:37:21 -0300

    initial commit for the Restaurants Reviews App

---
+++ App.js
@@ -0,0 +1,70 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import HeaderStyle from './HeaderStyle.js';
+
+const restaurants = [
+  {name: 'React Cafe', address: '123 Anywhere St'},
+  {name: 'Fancy restaurant', address: '123 Anywhere St'},
+  {name: 'Taco Place', address: '550 Maple Rd'}
+];
+export default class App extends Component {
+  render() {
+    return (
+        <View style={{flex: 1}}>
+          <Text style={HeaderStyle.header}> Restaurant Review</Text>
+          {
+            restaurants.map((place, index) => {
+              return (
+                  <View key={index} style={[
+                    styles.row,
+                    { backgroundColor: index % 2 === 0 ? 'white' : '#F3F3F7' }]
+                  }
+                  >
+                    <View style={styles.edges}>
+                      <Text>{index + 1}</Text>
+                    </View>
+
+                    <View style={styles.nameAddress}>
+                      <Text>{place.name}</Text>
+                      <Text style={styles.addressText}> {place.address} </Text>
+                    </View>
+
+                    <View style={styles.edges}>
+                      <Text>Info</Text>
+                    </View>
+
+                  </View>
+              )
+            })
+          }
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row'
+  },
+  edges: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 5
+  },
+  nameAddress: {
+    flexDirection: 'column',
+    flex: 8
+  },
+  addressText : {
+    color: 'grey'
+  }
+})
